98d1de9a5642db180a9068364c9fc8850adcfaf2
DavidAlmeida55 <184397944+DavidAlmeida55@users.noreply.github.com>
2024-11-29 10:31:37 +0000

    Update navegacao.php

--- navegacao.php
+++ navegacao.php
@@ -1,13 +1,13 @@
 <?php
-    include_once('verificar_sessao.php');
+   include_once('verificar_sessao.php');
 ?>
 <nav class="navbar bg-navegacao rounded">
-    <div class="container-fluid">
+   <div class="container-fluid">
         <span>Bem vindo <i><?php echo $_SESSION['utilizador']; ?></i></span>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" >
+       <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" >
             <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Opções</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
@@ -18,17 +18,22 @@
                     <button class="btn btn-outline-success" type="submit">Pesquisar</button>
                 </form>
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                   <?php 
+                   if ($_SESSION['tipo'] == 0) { ?>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
                             Administração
-                        </a>
+                       </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="criar_utilizador.php">Criar utilizador</a></li>
                             <li><a class="dropdown-item" href="utilizadores.php">Ver utilizadores</a></li>
                         </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="criar_topico.php">Criar tópico</a>
+                   </li>
+                <?php
+                }
+                ?>                  
+                  <li class="nav-item">
+                       <a class="nav-link" href="criar_topico.php">Criar tópico</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" href="topicos.php">Ver tópicos</a>
@@ -38,7 +43,7 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="terminar.php">Sair</a>
-                    </li>
+                   </li>
                 </ul>
             </div>
         </div>
